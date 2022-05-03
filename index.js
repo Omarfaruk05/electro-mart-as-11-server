ddbf0412fc32af9ee2fa3d8438264d44a79b37f6
Omarfaruk05 <mdomarfrauk149518.com>
2022-05-03 23:43:42 +0600

    adding gitignore

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ app.use(express.json());
 //electroMart
 //QXSy6evzp9RpaJyK
 
-// DB_USER=electroMart
-// DB_PASS=QXSy6evzp9RpaJyK 
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muwf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
