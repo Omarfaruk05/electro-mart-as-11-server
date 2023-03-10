f8fa1600e03e7e02ff427f0664163a362110ded1
Omarfaruk05 <mdomarfrauk149518.com>
2023-03-10 21:37:47 +0600

    vercel

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const query = require('express/lib/middleware/query');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const query = require("express/lib/middleware/query");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,110 +11,108 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-function verifyJWT(req, res, next){
-    const authHeader  = req.headers.authorization;
-    if(!authHeader){
-      return res.status(401).send({message: 'Unauthorized Access'})  
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbiden Access" });
     }
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=> {
-      if(err){
-        return res.status(403).send({message: 'Forbiden Access'})  
-      }  
-      console.log('decoded', decoded)
-      req.decoded = decoded;
-      next()
-    })
-    
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
 }
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muwf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 async function run() {
-    try{
-        await client.connect();
-        const productsCollection = client.db('electro-mart').collection('products');
+  try {
+    await client.connect();
+    const productsCollection = client.db("electro-mart").collection("products");
 
-        app.post('/login', (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
-                expiresIn: '1d'
-            })
-            res.send({token});
-        })
-        app.get('/product', async(req, res) => {
-            const query = {};
-            const cursor = productsCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
-        });
-        app.get('/addedProduct', verifyJWT, async(req, res) => {
-            const decodedEmail = req.decoded.email
-            const email = req.query.email;
-            console.log(email , decodedEmail)
-            if(email === decodedEmail){
-                const query = {email: email};
-                const cursor = productsCollection.find(query);
-                const addedProducts = await cursor.toArray();
-                res.send(addedProducts);
-            }
-            else{
-                res.status(403).send({message: 'Forbiden Email'})
-            }
-        });
+    app.post("/login", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ token });
+    });
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    app.get("/addedProduct", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
+      console.log(email, decodedEmail);
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = productsCollection.find(query);
+        const addedProducts = await cursor.toArray();
+        res.send(addedProducts);
+      } else {
+        res.status(403).send({ message: "Forbiden Email" });
+      }
+    });
 
-        app.get('/product/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const product = await productsCollection.findOne(query);
-            res.send(product);
-        });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
 
-        app.post('/product', async(req, res)=> {
-            const newService = req.body;
-            const result = await productsCollection.insertOne(newService);
-            res.send(result);
-        });
+    app.post("/product", async (req, res) => {
+      const newService = req.body;
+      const result = await productsCollection.insertOne(newService);
+      res.send(result);
+    });
 
-        app.put('/product/:id', async(req, res) => {
-            const id = req.params.id;
-            const updatedQuantity = req.body;
-            console.log(updatedQuantity)
-            const fliter = {_id: ObjectId(id)};
-            const options = {upsert: true};
-            const updatedDoc = {
-                $set:{
-                    quantity : updatedQuantity.quantity
-                }
-            };
-            const result = await productsCollection.updateOne(fliter, updatedDoc, options);
-            res.send(result);
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedQuantity = req.body;
+      console.log(updatedQuantity);
+      const fliter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          quantity: updatedQuantity.quantity,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        fliter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
 
-        })
-
-        app.delete('/product/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const result =  await productsCollection.deleteOne(query);
-            res.send(result);
-        });
-    }
-
-    finally{
-
-    }
-};
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('running electro mart')
+app.get("/", (req, res) => {
+  res.send("running electro mart");
 });
 
 app.listen(port, () => {
-    console.log('listening to port', port)
-})
-
-
+  console.log("listening to port", port);
+});
